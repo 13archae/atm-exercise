423d0cda282817a6f1edc81e48e16b6bd74e10ae
archae13 <archae13@lpbroadband,net>
2024-04-30 15:02:56 -0600

    Baseline complete with H3 not working

--- account1.jsx
+++ account1.jsx
@@ -1,9 +1,10 @@
 // this keeps a running total of deposits and withdrawals
 
-const ATMDeposit = ({ onChange }) => {
+const ATMDeposit = ({ onChange, isDeposit = true }) => {
+  const choice = ["Deposit", "Cash Back"];
   return (
     <label className="label huge">
-      Deposit:
+      <h3>{choice[Number(isDeposit)]}</h3>
       <input type="number" onChange={onChange}></input>
       <input type="submit"></input>
     </label>
@@ -12,14 +13,15 @@ const ATMDeposit = ({ onChange }) => {
 
 const Account = () => {
   const [accountState, setAccountState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
   var deposit = 0;
-  const handleChange = event => {
+  const handleChange = (event) => {
     console.log(`handleChange ${event.target.value}`);
     deposit = Number(event.target.value);
   };
-  const handleSubmit = event => {
-    let newTotal = accountState + deposit;
-    alert(`Account total = ${newTotal}`);
+  const handleSubmit = (event) => {
+    let newTotal = isDeposit ? accountState + deposit : accountState - deposit;
+    //alert(`Account total = ${newTotal}`);
     setAccountState(newTotal);
     event.preventDefault();
   };
@@ -27,8 +29,25 @@ const Account = () => {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Account Balance {accountState}</h2>
+
       <button
-      <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
+        onClick={(event) => {
+          setIsDeposit(true);
+          event.preventDefault();
+        }}
+      >
+        Deposit
+      </button>
+      <button
+        onClick={(event) => {
+          setIsDeposit(false);
+          event.preventDefault();
+        }}
+      >
+        Cash Back
+      </button>
+
+      <ATMDeposit onChange={handleChange}> isDeposit={isDeposit}</ATMDeposit>
     </form>
   );
 };
