94c42b360cd8f990cb3d0e4505bfb7191899da00
archae13 <archae13@lpbroadband,net>
2024-04-30 16:37:16 -0600

    Refactor to hide form if no ATM mode selected

--- account1.jsx
+++ account1.jsx
@@ -1,25 +1,37 @@
 // this keeps a running total of deposits and withdrawal
-const ATMDeposit = ({ onChange, isDeposit }) => {
+const ATMDeposit = ({ onChange, isDeposit, atmMode }) => {
   const choice = ["Deposit", "Cash Back"];
   console.log(`ATM isDeposit: ${isDeposit}`);
-  return (
-    <label className="label huge">
-      <h3> {choice[Number(!isDeposit)]}</h3>
-      <input
-        id="number-input"
-        type="number"
-        width="200"
-        onChange={onChange}
-      ></input>
-      <input type="submit" width="200" value="Submit" id="submit-input"></input>
-    </label>
-  );
+  if (atmMode === "") {
+    return <div></div>;
+  } else {
+    return (
+      <div>
+        <label className="label huge">
+          <h3> {choice[Number(!isDeposit)]}</h3>
+          <input
+            id="number-input"
+            type="number"
+            width="200"
+            onChange={onChange}
+          ></input>
+          <input
+            type="submit"
+            width="200"
+            value="Submit"
+            id="submit-input"
+          ></input>
+        </label>
+      </div>
+    );
+  }
 };
 
 const Account = () => {
   const [accountState, setAccountState] = React.useState(0);
   const [deposit, setDeposit] = React.useState(0);
   const [isDeposit, setIsDeposit] = React.useState(true);
+  const [atmMode, setAtmMode] = React.useState("");
   console.log(`isDeposit in account: ${isDeposit}`);
 
   const handleChange = (event) => {
@@ -32,29 +44,41 @@ const Account = () => {
     setAccountState(newTotal);
     event.preventDefault();
   };
+  const handleModeSelect = (event) => {
+    const selectvalue = event.target.value;
+    setAtmMode(selectvalue);
+    if (selectvalue === "Deposit") {
+      setIsDeposit(true);
+    } else if (selectvalue === "Cash Back") {
+      setIsDeposit(false);
+    }
+
+    event.preventDefault();
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Account Balance {accountState}</h2>
-
-      <button
-        onClick={(event) => {
-          setIsDeposit(true);
-          event.preventDefault();
-        }}
+      <label>Select an action below to continue</label>
+      <select
+        onChange={(e) => handleModeSelect(e)}
+        name="mode"
+        id="mode-select"
       >
-        Deposit
-      </button>
-      <button
-        onClick={(event) => {
-          setIsDeposit(false);
-          event.preventDefault();
-        }}
-      >
-        Cash Back
-      </button>
+        <option id="no-selection" value=""></option>
+        <option id="deposit-selection" value="Deposit">
+          Deposit
+        </option>
+        <option id="cashback-selection" value="Cash Back">
+          Cash Back
+        </option>
+      </select>
 
-      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}></ATMDeposit>
+      <ATMDeposit
+        onChange={handleChange}
+        isDeposit={isDeposit}
+        atmMode={atmMode}
+      ></ATMDeposit>
     </form>
   );
 };
